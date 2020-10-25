b73c0ff0f70a9b8412184dac6f88acab3523c90d
Nandhini <70878131+nandhini130@users.noreply.github.com>
2020-10-25 19:47:09 +0530

    [nandhini - 2510] - Initial Commit

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var s;
+var scl = 20;
+var food;
+
+function setup() {
+  createCanvas(600, 600);
+  s = new Snake();
+  frameRate(10);
+  picklocation();
+}
+
+function picklocation()  {
+  var rows = floor(height/scl);
+  var cols = floor(width/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+  
+}
+
+function draw() {
+  background(51);
+  s.death();
+  s.update();
+  s.show();
+  
+  if(s.eat(food)){
+    picklocation();
+  }
+  
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+}
+
+function keyPressed() {
+  if(keyCode === UP_ARROW) {
+    s.dir(0, -1)
+  } else if(keyCode === DOWN_ARROW) {
+    s.dir(0, 1)
+  } else if(keyCode === RIGHT_ARROW) {
+    s.dir(1, 0)
+  } else if(keyCode == LEFT_ARROW) {
+    s.dir(-1, 0)
+  }
+}
